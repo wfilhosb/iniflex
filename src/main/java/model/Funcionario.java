06b41d7d7e90bb152218e0a4fb46b1179d60cd63
wfilhosb <wfilhosb@hotmail.com>
2024-06-19 17:30:24 -0300

    Ajuste nas classes.

--- src/main/java/model/Funcionario.java
+++ src/main/java/model/Funcionario.java
@@ -2,9 +2,12 @@ package model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Funcionario extends Pessoa {
 	private BigDecimal salario;
--- src/main/java/model/Pessoa.java
+++ src/main/java/model/Pessoa.java
@@ -1,9 +1,12 @@
 package model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 public class Pessoa {
 	private String nome;
 	private Date dataNascimento;
