bcec18ddccb14a6835afe8488e259ca3ef788d63
wfilhosb <wfilhosb@hotmail.com>
2024-06-19 17:31:06 -0300

    Classes Dao específicas de cada classe.

---
+++ src/main/java/dao/DaoFuncionario.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class DaoFuncionario extends Dao<DaoFuncionario> {
+    public DaoFuncionario() {
+        super(DaoFuncionario.class);
+    }
+}
---
+++ src/main/java/dao/DaoPessoa.java
@@ -0,0 +1,9 @@
+package dao;
+
+import model.Pessoa;
+
+public class DaoPessoa extends Dao<Pessoa> {
+    public DaoPessoa() {
+        super(Pessoa.class);
+    }
+}
